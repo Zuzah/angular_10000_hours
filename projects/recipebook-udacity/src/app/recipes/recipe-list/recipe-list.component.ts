3a66f822b888b788479b01018660c033c2d41eae
Zuzah <murtaza.hasni@gmail.com>
2021-01-31 15:02:01 -0500

    generate components to house recipe and images

--- projects/recipebook-udacity/src/app/recipes/recipe-list/recipe-list.component.ts
+++ projects/recipebook-udacity/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../shared/models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipies: Recipe[] = [
+    new Recipe('Red Velvet Cake', 'A vanilla cake with red colouring', '../../assets/red-velvet-cake.jpg'),
+
+    new Recipe('Fudge Cake Cake', 'A chocolate cake with extra chocolate', '../../assets/fudge-cake.jpg'),
+  ];
 
   constructor() { }
 
